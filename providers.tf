82490026a0dc9156d1f893d45b985e00b8a052f6
jaderdomonte <30699505+jaderdomonte@users.noreply.github.com>
2023-01-25 21:13:23 -0300

    First commit

---
+++ providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">=0.13.1" 
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">=4.51.0"
+    }
+    local = ">=2.1.0"
+  }
+}
+
+provider "aws" {
+    region = "sa-east-1"
+}
